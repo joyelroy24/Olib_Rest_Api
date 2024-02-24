a607357650d446837a0f2cf65dc786c27918f4e7
joyel <joyelroy24@gmail.com>
2024-02-24 17:59:44 +0530

    solved issue on reader client

--- client/reader.py
+++ client/reader.py
@@ -31,40 +31,40 @@ if auth_response.status_code==200:
         textfordisplay_review_authors=''
 
         # create a text identitfy author and pk to user input management
-        if get_response.json()['results']:
-            print("**************")
-            review_or_viewReviews=int(input("enter 1 for view reviews enter 2 add reviews :"))
-            for i in get_response.json()['results']:
-                text=f" Enter {i['pk']} for author {i['name']}, "
-                textfordisplay_review_authors=textfordisplay_review_authors+text
-            textfordisplay_review_authors=textfordisplay_review_authors+" : "
+        
+        print("**************")
+        review_or_viewReviews=int(input("enter 1 for view reviews enter 2 add reviews :"))
+        for i in get_response.json():
+            text=f" Enter {i['pk']} for author {i['name']}, "
+            textfordisplay_review_authors=textfordisplay_review_authors+text
+        textfordisplay_review_authors=textfordisplay_review_authors+" : "
 
-            # display the text with author and corresponding id's to enter input from user
-            # ;here user will enter pk of author using that we can view author specifc reviews
-            reviews_about_author=int(input(textfordisplay_review_authors)) 
+        # display the text with author and corresponding id's to enter input from user
+        # ;here user will enter pk of author using that we can view author specifc reviews
+        reviews_about_author=int(input(textfordisplay_review_authors)) 
 
-            if review_or_viewReviews==1:
-                endpoint = "http://localhost:8000/authorvise_review/" 
-                # add pk of user to endpont
-                endpoint=endpoint+str(reviews_about_author)
-                get_response = requests.get(endpoint,headers=headers) 
-                print("...........Reviews............... \n ")
-                print(get_response.json())
-                
-            else:
-                # option for add review 
-                review=input("enter review :")
-                rate=int(input("enter rate out of 5 :"))
-                endpoint = "http://localhost:8000/review_author_create" 
-                
-                
+        if review_or_viewReviews==1:
+            endpoint = "http://localhost:8000/authorvise_review/" 
+            # add pk of user to endpont
+            endpoint=endpoint+str(reviews_about_author)
+            get_response = requests.get(endpoint,headers=headers) 
+            print("...........Reviews............... \n ")
+            print(get_response.json())
             
-                get_response = requests.post(endpoint,headers=headers,json={"review":review,'rating':rate,'author':reviews_about_author}) 
-                 
-                if auth_response.status_code==200:
-                    print("....Review Added.....")
-                 
-                    print(get_response.json())   
+        else:
+            # option for add review 
+            review=input("enter review :")
+            rate=int(input("enter rate out of 5 :"))
+            endpoint = "http://localhost:8000/review_author_create" 
+            
+            
+        
+            get_response = requests.post(endpoint,headers=headers,json={"review":review,'rating':rate,'author':reviews_about_author}) 
+                
+            if auth_response.status_code==200:
+                print("....Review Added.....")
+                
+                print(get_response.json())   
             
                 
     elif option==2:
